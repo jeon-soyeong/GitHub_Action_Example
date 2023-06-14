53c492417e5c5559d12f4fdfc7e60c1a222c3bbe
전소영 <jsu3417@gmail.com>
2023-06-14 22:28:10 +0900

    프로젝트 생성

---
+++ GitHub_Action_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub_Action_Example
+//
+//  Created by 전소영 on 2023/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
